0e164a091d0603e725372a5869f8fb458d20ea6d
Julio Barrios <juliobarmi@gmail.com>
2017-05-06 16:59:06 -0300

    Adds more descriptive titles

--- app/routes/meetup.js
+++ app/routes/meetup.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
 
-const { Route, inject: { service } } = Ember;
+const { Route, inject: { service }, RSVP, String: { capitalize } } = Ember;
 
 export default Route.extend({
-  meetup:service(),
+  meetup: service(),
 
   model({ slug }) {
-    return this.get('meetup').getMeetupInfo(slug);
+    return new RSVP.hash({
+      title: capitalize(moment(slug).format('MMMM YYYY')),
+      content: this.get('meetup').getMeetupInfo(slug)
+    });
   }
 });
